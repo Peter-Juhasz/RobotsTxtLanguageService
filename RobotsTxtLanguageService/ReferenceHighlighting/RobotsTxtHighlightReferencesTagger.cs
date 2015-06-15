4718e69f8d0c26c5b356f517ff93f8538cc59567
Peter-Juhasz <peter.juhasz@live.com>
2015-06-15 23:42:04 +0200

    Reference highlight

---
+++ RobotsTxtLanguageService/ReferenceHighlighting/RobotsTxtHighlightReferencesTagger.cs
@@ -0,0 +1,79 @@
+﻿using RobotsTxtLanguageService.Syntax;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace RobotsTxtLanguageService
+{
+    [Export(typeof(IViewTaggerProvider))]
+    [TagType(typeof(ITextMarkerTag))]
+    [ContentType(RobotsTxtContentTypeNames.RobotsTxt)]
+    internal sealed class RobotsTxtHighlightReferencesTaggerProvider : IViewTaggerProvider
+    {
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            return textView.Properties.GetOrCreateSingletonProperty(
+                creator: () => new RobotsTxtHighlightReferencesTagger(textView)
+            ) as ITagger<T>;
+        }
+
+
+        private sealed class RobotsTxtHighlightReferencesTagger : ITagger<ITextMarkerTag>
+        {
+            public RobotsTxtHighlightReferencesTagger(ITextView view)
+            {
+                _view = view;
+
+                _view.Caret.PositionChanged += OnCaretPositionChanged;
+            }
+
+            private readonly ITextView _view;
+
+            private static readonly ITextMarkerTag Tag = new TextMarkerTag("MarkerFormatDefinition/HighlightedReference");
+
+
+            private void OnCaretPositionChanged(object sender, CaretPositionChangedEventArgs e)
+            {
+                // TODO: optimize changed spans
+                this.TagsChanged?.Invoke(this,
+                    new SnapshotSpanEventArgs(new SnapshotSpan(e.TextView.TextBuffer.CurrentSnapshot, 0, e.TextView.TextBuffer.CurrentSnapshot.Length))
+                );
+            }
+            
+            public IEnumerable<ITagSpan<ITextMarkerTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+            {
+                ITextBuffer buffer = spans.First().Snapshot.TextBuffer;
+                SyntaxTree syntax = buffer.GetSyntaxTree();
+                RobotsTxtDocumentSyntax root = syntax.Root as RobotsTxtDocumentSyntax;
+                
+                SnapshotPoint caret = _view.Caret.Position.BufferPosition;
+
+                // find section
+                RobotsTxtRecordSyntax record = root.Records
+                    .FirstOrDefault(s => s.NameToken.Span.Span.Contains(caret));
+
+                // show references
+                if (record != null)
+                {
+                    string recordName = record.NameToken.Value;
+
+                    // find references
+                    return
+                        from r in root.Records
+                        where r.NameToken.Value.Equals(recordName, StringComparison.InvariantCultureIgnoreCase)
+                        select new TagSpan<ITextMarkerTag>(r.NameToken.Span.Span, Tag)
+                    ;
+                }
+                
+                return Enumerable.Empty<TagSpan<ITextMarkerTag>>();
+            }
+
+            public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+        }
+    }
+}
